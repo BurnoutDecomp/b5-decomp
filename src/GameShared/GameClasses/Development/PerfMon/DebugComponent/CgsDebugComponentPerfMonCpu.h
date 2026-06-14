19ff8bb7c266582ed72ba0bb55ecf9f2f7c631b1
Adriwin <76881633+Adriwin06@users.noreply.github.com>
2026-06-14 15:29:39 +0200

    Improve one file using DWARF dump

--- src/GameShared/GameClasses/Development/PerfMon/DebugComponent/CgsDebugComponentPerfMonCpu.h
+++ src/GameShared/GameClasses/Development/PerfMon/DebugComponent/CgsDebugComponentPerfMonCpu.h
@@ -11,11 +11,12 @@ namespace CgsDev
 class DebugComponentPerfMonCpu : public DebugComponent
 {
 public:
-    void Construct( int16_t liMaxMonitors );
+    void Construct( int16_t liMaxMonitors, void* lpLogBuffer, uint32_t luLogBufferSize );
     void Destruct();
     virtual void RenderHUD( Debug2DImmediateRender* lpDebug2DRender );
     virtual void Update();
     void SetPageName( int32_t liPage, const char* lpcPageName );
+    void GetCurrentPage( int32_t* lpiPage, char* lpcName, int32_t liNameLen );
 
 protected:
     virtual const char* GetName() const { return "CPU Monitors"; }
@@ -30,6 +31,7 @@ protected:
 
     bool mbResetMonitors;
     static void DebugCallbackResetCounters( void* );
+    static void DebugCallbackDumpLogFile( void* );
 
     bool mbEnableTracing;
     static void DebugEnableTracingCallback(void* lpValue,void* lpUserData);
@@ -42,6 +44,16 @@ protected:
 
     int32_t miCurrentPage;
     DebugUI::StringList maStringList[E_PMP_MAX+2];
+
+    // Optional CPU-trace log buffer (filled when tracing is enabled).
+    void LogBufferEnable( void* lpBuffer, int32_t liSize );
+    void LogBufferUpdate();
+    void LogBufferDump( char* lpcFileName );
+
+    void* mpLogBuffer;
+    void* mpLogBufferEnd;
+    bool mbLogBufferOverflow;
+    uint16_t* mpau16LogBufferWritePtr;
 };
 
 }
